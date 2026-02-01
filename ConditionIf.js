2b977d3d950824f82d940144e6fd1157a3c10a96
Monica B <monicasathya147@gmail.com>
2026-02-01 22:55:14 +0530

    Add files via upload

---
+++ ConditionIf.js
@@ -0,0 +1,43 @@
+//if else
+let bicycle=false
+
+if(bicycle==true){
+console.log("I will go to school");
+}
+else{
+   console.log("I will not go to school"); 
+}
+
+
+//esle if
+let day="saturday"
+
+if(day=="monday"){
+    console.log("I will go to school");
+}
+else if(day=="saturday"){
+console.log("this is an weekend")
+}
+else{
+    console.log("entered a valid day");
+}
+
+
+//nested if
+let degree="BE"
+let percentage=71.2
+
+if(degree=="B.tech" || degree=="BE"){
+console.log("I am having BE or B.Tech Degree...");
+if(percentage >=90 && percentage<=100){
+console.log("you are eligible to apply in Infosys");
+}
+else{
+console.log("Not eligible");
+}
+}
+else{
+console.log("only BE or BTech candidate applicable");
+}
+
+
