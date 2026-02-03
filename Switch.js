ea452132c97f5d9d0faae3fb5083f3e83132b1a0
Monica B <monicasathya147@gmail.com>
2026-02-03 22:40:08 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,31 @@
+let day =1
+switch(day){
+    case 1:
+    console.log("I am Monday");
+    break;
+
+    case 2:
+    console.log("I am Tuesday");
+    break;
+    
+    case 3:
+    console.log("I am Wednesday");
+    break;
+    
+    case 4:
+    console.log("I am Thursday");
+    break;
+    
+    case 5:
+    console.log("I am Friday");
+    break;
+    
+    case 6:
+    console.log("I am Saturday");
+    break;
+    
+    case 7:
+    console.log("I am Sunday");
+    break;
+    
+}
